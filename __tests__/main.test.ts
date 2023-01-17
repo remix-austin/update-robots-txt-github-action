04673da6e2d4c2a33fa1308e936a46e09edf1724
Grant Norwood <norwood.grant@heb.com>
2023-01-17 02:14:51 -0600

    Rember to run `npm run all` before pushing to PR branch

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -4,7 +4,13 @@ import * as cp from 'child_process'
 import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
-test('wait 500 ms', async () => {
+// TODO: Delete these old test examples
+test.skip('throws invalid number', async () => {
+  const input = parseInt('foo', 10)
+  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+})
+
+test.skip('wait 500 ms', async () => {
   const start = new Date()
   await wait(500)
   const end = new Date()
@@ -12,27 +18,13 @@ test('wait 500 ms', async () => {
   expect(delta).toBeGreaterThan(450)
 })
 
-// TODO: Delete these old test examples
-// test.skip('throws invalid number', async () => {
-//   const input = parseInt('foo', 10)
-//   await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-// })
-
-// test.skip('wait 500 ms', async () => {
-//   const start = new Date()
-//   await wait(500)
-//   const end = new Date()
-//   var delta = Math.abs(end.getTime() - start.getTime())
-//   expect(delta).toBeGreaterThan(450)
-// })
-
-// // shows how the runner will run a javascript action with env / stdout protocol
-// test.skip('test runs', () => {
-//   process.env['INPUT_MILLISECONDS'] = '500'
-//   const np = process.execPath
-//   const ip = path.join(__dirname, '..', 'lib', 'main.js')
-//   const options: cp.ExecFileSyncOptions = {
-//     env: process.env
-//   }
-//   console.log(cp.execFileSync(np, [ip], options).toString())
-// })
+// shows how the runner will run a javascript action with env / stdout protocol
+test.skip('test runs', () => {
+  process.env['INPUT_MILLISECONDS'] = '500'
+  const np = process.execPath
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecFileSyncOptions = {
+    env: process.env
+  }
+  console.log(cp.execFileSync(np, [ip], options).toString())
+})
